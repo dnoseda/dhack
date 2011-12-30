41a880897110d3ecc7ce393766c1b9b96902be3e
dnoseda <damian.noseda@mercadolibre.com>
2011-12-30 17:04:56 -0300

    fix run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-groovy -classpath "lib/json-lib-2.4-jdk15.jar:lib/commons-lang-2.4.jar:lib/ezmorph-1.0.6.jar:lib/commons-beanutils-1.8.0.jar" src/Search.groovy
+groovy -classpath "lib/json-lib-2.4-jdk15.jar:lib/commons-lang-2.4.jar:lib/ezmorph-1.0.6.jar:lib/commons-beanutils-1.8.0.jar:lib/commons-collections-3.2.1.jar" src/Search.groovy
