b1813d0a2e1d4e4a742c931348835763a7fe8d74
dnoseda <damian.noseda@mercadolibre.com>
2012-04-23 11:00:14 -0300

    with range

--- src/Search.groovy
+++ src/Search.groovy
@@ -17,44 +17,48 @@ class Search {
 	static main(args) {
 		
 		
-		def initial = new GregorianCalendar(2012, Calendar.JUNE, 10, 23, 59)
+		def initial = new GregorianCalendar(2012, Calendar.NOVEMBER, 1, 23, 59)
 		def results = [:]
-		def range = 2
+		def range = 30
+		def lambda = 4
 		def daysBetween = 15
 		def show = { k,item ->
 			println "$k\t${item.stops}\t${item.price}\t${item.from}\t${item.to}\t${item.dest}"
 		}
 
 		for(int i=0; i < 10; i++){
-			for(String dest: [ "BCN", "ROM", "LON","LIS", "MAD"]){
+			for(String dest: [ "MEX"]){
 				def from = initial.clone()
 				from.add(Calendar.DATE, i)
 				def to = from.clone()
-				to.add(Calendar.DATE, daysBetween)
-				def froms = String.format('%tF', from)
-				def tos = String.format('%tF', to)
-				def url = "http://www.despegar.com.ar/Flights.Services/Flights/Flights.svc/ClusteredFlights/BUE/${dest}/${froms}/${tos}/1/0/0"
-				def fUrl = "http://www.despegar.com.ar/search/flights/RoundTrip/BUE/${dest}/${froms}/${tos}/1/0/0"
-				if(!results[fUrl]){
-					println "a $dest fecha: $froms volviendo el $tos..."
-					try{
-						def searchResult = JSONObject.fromObject (url.toURL().getText())
-						results[fUrl] = searchResult.Boxs.collect({
-							println "DN price :${it.Itns[0].Tot?.Loc} class ${it.Itns[0].Tot?.Loc.getClass()}"
-							[
-								stops:it.Dep[0].Segmts?.Stops?.size(), 
-								from:froms,
-								to:tos,
-								price:it.Itns[0].Tot?.Loc,
-								dest:dest
-							]
-						})
-						results[fUrl].each{v ->
-							print "parcial: "
-							show(fUrl,v)
+				for(j in [(lambda*-1)..lambda]){
+					daysBetween += j
+					to.add(Calendar.DATE, daysBetween)
+					def froms = String.format('%tF', from)
+					def tos = String.format('%tF', to)
+					def url = "http://www.despegar.com.ar/Flights.Services/Flights/Flights.svc/ClusteredFlights/BUE/${dest}/${froms}/${tos}/1/0/0"
+					def fUrl = "http://www.despegar.com.ar/search/flights/RoundTrip/BUE/${dest}/${froms}/${tos}/1/0/0"
+					if(!results[fUrl]){
+						println "a $dest fecha: $froms volviendo el $tos..."
+						try{
+							def searchResult = JSONObject.fromObject (url.toURL().getText())
+							results[fUrl] = searchResult.Boxs.collect({
+								println "DN price :${it.Itns[0].Tot?.Loc} class ${it.Itns[0].Tot?.Loc.getClass()}"
+								[
+									stops:it.Dep[0].Segmts?.Stops?.size(), 
+									from:froms,
+									to:tos,
+									price:it.Itns[0].Tot?.Loc,
+									dest:dest
+								]
+							})
+							results[fUrl].each{v ->
+								print "parcial: "
+								show(fUrl,v)
+							}
+						}catch(Exception e){
+							println e
 						}
-					}catch(Exception e){
-						println e
 					}
 				}
 			}
